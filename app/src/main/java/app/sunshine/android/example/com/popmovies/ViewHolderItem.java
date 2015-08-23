cc43f2888365345637e62dbea18afa755961f306
Sharath Koday <sharath1608@gmail.com>
2015-08-22 23:01:27 -0700

    Added GridViewObject and ViewHolderItem classes which was missed during the last push

---
+++ app/src/main/java/app/sunshine/android/example/com/popmovies/ViewHolderItem.java
@@ -0,0 +1,17 @@
+package app.sunshine.android.example.com.popmovies;
+
+import android.media.Image;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+/**
+ * Created by Asus1 on 8/18/2015.
+ */
+public class ViewHolderItem {
+
+    public ImageView movieImage;
+    public TextView movieTag;
+    public ProgressBar progressBar;
+
+}
